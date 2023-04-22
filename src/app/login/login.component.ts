2d1fbdb979bffd1c420a6eddb1323464fa320699
Yesith <108515089+yesith94@users.noreply.github.com>
2023-04-22 23:23:45 +0100

    ADD: components and services

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit{
+
+  form: FormGroup;
+  private formSubmitAttempt: boolean ;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
+  ngOnInit(): void {
+  }
+
+  isFieldInvalid(field: string) { // {6}
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+     // this.authService.login(this.form.value); // {7}
+    }
+    this.formSubmitAttempt = true;             // {8}
+  }
+
+}
