89d14a7f55f2f39e3a650e8c421f81f4a601af1a
Yesith <108515089+yesith94@users.noreply.github.com>
2023-04-29 11:27:07 +0100

    ADD: student registration form material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
+import { RegistrationComponent } from './registration/registration.component';
 
 
 
@@ -20,7 +21,8 @@ import {MatIconModule} from "@angular/material/icon";
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
